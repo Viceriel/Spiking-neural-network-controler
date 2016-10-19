0229dd5141b4ba34fad30eb3c488da3656b569ad
Viceriel <viceriel@gmail.com>
2016-10-19 16:13:30 +0200

    Repo init

---
+++ SpikeNeuron.m
@@ -0,0 +1,19 @@
+classdef SpikeNeuron
+   
+    properties
+       
+        thalamitic_input;
+        output;
+        u;
+        v;
+        weights;
+        
+    end
+    
+    methods
+        
+        function obj = SpikeNeuron(connections)
+            
+        end
+    end
+end

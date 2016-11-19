e8eb6cba0f19516cceeba9a667b7986900a85605
Viceriel <viceriel@gmail.com>
2016-11-19 12:37:53 +0100

    Fix of fitness function and initialization

--- Main.m
+++ Main.m
@@ -13,7 +13,7 @@ global fi
 % RunSim(net, 0.1, 0.1, 10);
 
 EA = GeneticAlgorithm(0.05, 3, [6 5 2], [0 0 0; 1 0 0; 1 1 0]);
-EA = Evolve(EA, 5);
+EA = Evolve(EA, 3);
 top = GetBest(EA, EA.m_population);
 net = Decoding(EA, top, EA.m_population);
 

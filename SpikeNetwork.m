4e41ab07481516aaed911b74e86d87d9cea5cb68
Jakub Kajan <viceriel@gmail.com>
2017-10-21 09:04:11 +0200

    Commented

--- SpikeNetwork.m
+++ SpikeNetwork.m
@@ -3,19 +3,23 @@ classdef SpikeNetwork
     
     properties
        
-        neural;
-        weights;
-        connections;
-        layers;
-        time;
+        neural;     %neuron layers                
+        weights;    %synapse layers
+        connections; %connection matrix
+        layers;      %count of layers
+        time;        %time in network
         max;
         min;
-        net_output;
+        net_output;  %output vector of network
         
     end
     
     methods
        
+        %Network constructor
+        % structure- topology of network
+        %connections- connections between layers
+        %ma, mi- synapse bounding
         function obj = SpikeNetwork(structure, connections, ma, mi)
            
             obj.max = ma;
@@ -27,6 +31,9 @@ classdef SpikeNetwork
             obj.weights = {};
             obj.net_output = 0;
             
+            %crerating connections between layers according to connection argument
+            %every neuron(except input layer) has synapse vector in net topology
+            %so there are four cycles, two for layer and neuron in layer, and another two for same reason(see comment two)
             for i = 2 : len
                
                 obj.weights{i} = {};
@@ -55,6 +62,9 @@ classdef SpikeNetwork
                 
             end
             
+            %creating layers of neurons
+            %first layers is created from input neurons
+            %another layers are spiking neurons
             for i = 1 : len
                 
                 neurons = {};
@@ -79,10 +89,17 @@ classdef SpikeNetwork
             
         end
         
+        %running network with input data
+        %obj- network
+        %data- vector of input data- obsolete because input neurons are injected in RunSim
+        %return- network
         function obj = Run(obj, data)
             
             len = length(obj.layers);
             
+            %running through first layer
+            %if some neuron from first layer emitted spike, spike time is recorded
+            %if isn't emitted spike, spike time is zero and time from spike is incremented
             for i = 1 : obj.layers(1)     
                 obj.neural{1}{1}{i} = ComputeOutput(obj.neural{1}{1}{i});
                 if obj.neural{1}{1}{i}.output == 1
@@ -104,8 +121,11 @@ classdef SpikeNetwork
                 end                
             end
             
+            %passing through remaining layers
+            %after next layer, time is incremented
             for i = 2 : len
                 obj.time = obj.time + 0.1;
+                %computing input for neurons(output x synapse)
                 for j = 1 : obj.layers(i)
                     input = 0;
                     for k = 1 : len
@@ -116,9 +136,11 @@ classdef SpikeNetwork
                     
                     obj.neural{i}{1}{j} = OutputCompute(obj.neural{i}{1}{j}, input);
                     
+                    %if neuron emitt spike, spike time is recorded
                     if obj.neural{i}{1}{j}.output == 1
                         obj.neural{i}{1}{j}.spike_time = obj.time;
                         
+                        %recording spike time of neuron to synapses of neurons in next layer
                         if i ~= len
                            for x = i + 1 : len
                                for y = 1 : obj.layers(x)
@@ -127,7 +149,9 @@ classdef SpikeNetwork
                                end
                            end     
                         end
-                        
+                         
+                         %passing through all synapses of actual neuron
+                         %applying STDP based on spike times
                          for m = 1 : len
                             for n = 1 : obj.layers(m)  
                                 if obj.weights{i}{j}{m}{n}.value ~= 0
@@ -140,15 +164,17 @@ classdef SpikeNetwork
                                 end      
                             end        
                          end
-                         
+                        
+                        %reset of spike times
                         for x = i : -1 : 1
                             for y = 1 : obj.layers(x)
                                 obj.weights{x}{y}{i}{j}.spike_time = 0;
                                 obj.weights{i}{j}{m}{n}.time_from_spike = 0;
                             end
                         end
-                    else
+                    else %if no spike
                          if i ~= len
+                          %incrementing of time distance from last spike to synapse of next layer neuron
                            for x = i + 1 : len
                                for y = 1 : obj.layers(x)
                                    if obj.weights{x}{y}{i}{j}.spike_time == 0
@@ -161,8 +187,11 @@ classdef SpikeNetwork
                 end
             end
             
+            %this is because of first data pass, times in layers are 0.1, 0.2, 0.3
+            %we want in second samples 0.2, 0.3, 0.4
             obj.time = obj.time - 0.1;
             
+            %collecting  network output
             for i = 1 : obj.layers(len)
                
                 obj.net_output(i) = obj.neural{len}{1}{i}.output;
@@ -170,15 +199,17 @@ classdef SpikeNetwork
             end
         end
         
+        %Spike time dependent plasticity rule for updating synapses
+        %parameters of STDP are optimized by Genetic algorithm
         function obj = STDP(obj, spike_differences, post, pre, rule)
-            
+            %if using STDP
             if rule == 1
                 if spike_differences < 0 
                     obj.weights{post(1)}{post(2)}{pre(1)}{pre(2)}.value = obj.weights{post(1)}{post(2)}{pre(1)}{pre(2)}.value + obj.neural{post(1)}{1}{post(2)}.A1*exp(spike_differences / obj.neural{post(1)}{1}{post(2)}.t);
                 else
                     obj.weights{post(1)}{post(2)}{pre(1)}{pre(2)}.value = obj.weights{post(1)}{post(2)}{pre(1)}{pre(2)}.value - obj.neural{post(1)}{1}{post(2)}.A2*exp(-spike_differences / obj.neural{post(1)}{1}{post(2)}.t);
                 end
-            else
+            else %ANTI STDP
                 if spike_differences < 0
                     obj.weights{post(1)}{post(2)}{pre(1)}{pre(2)}.value = obj.weights{post(1)}{post(2)}{pre(1)}{pre(2)}.value - obj.neural{post(1)}{1}{post(2)}.A2*exp(-spike_differences / obj.neural{post(1)}{1}{post(2)}.t);
                 else
@@ -186,6 +217,7 @@ classdef SpikeNetwork
                 end
             end
             
+            %bounding of weight change
             if obj.weights{post(1)}{post(2)}{pre(1)}{pre(2)}.value > obj.max
                 obj.weights{post(1)}{post(2)}{pre(1)}{pre(2)}.value = obj.max;
             elseif obj.weights{post(1)}{post(2)}{pre(1)}{pre(2)}.value < obj.min
